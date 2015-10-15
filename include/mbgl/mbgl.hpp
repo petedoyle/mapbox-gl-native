b98ef1c08163db4616e639d5d1fb54b170a66bc8
Mike Morris <michael.patrick.morris@gmail.com>
2015-10-15 13:59:53 -0400

    define MBGL_PUBLIC and MBGL_LOCAL for symbol visibility

--- include/mbgl/mbgl.hpp
+++ include/mbgl/mbgl.hpp
@@ -1,6 +1,9 @@
 #ifndef MBGL_MAIN
 #define MBGL_MAIN
 
+#define MBGL_PUBLIC __attribute__ ((visibility ("default")))
+#define MBGL_LOCAL  __attribute__ ((visibility ("hidden")))
+
 #include "map/map.hpp"
 #include "map/view.hpp"
 
